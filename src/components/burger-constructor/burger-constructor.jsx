d96a186e4d57ed90222f4d6a3fd30aa9e0974764
Ekaterina Polekhova <k.polexowa@yandex.ru>
2024-05-20 14:46:27 +0300

    Слайдер у конструктора добавила

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -6,7 +6,7 @@ function BurgerConstructor() {
 
     return (
         <section className="pt-25" style={{maxWidth: 600, width: "100%"}}>
-            <div className="pr-4 pl-4" style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <div className="pr-4 pl-4" style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
                 <div className={ styles.drag_div }>
                     <ConstructorElement
                         type="top"
@@ -17,13 +17,63 @@ function BurgerConstructor() {
                     />
                 </div>
                 
-                <div className={ styles.drag_div }>
-                    <DragIcon type="primary" />
-                    <ConstructorElement
-                        text="Краторная булка N-200i (верх)"
-                        price={50}
-                        thumbnail="https://code.s3.yandex.net/react/code/bun-02.png"
-                    />
+                <div className={ styles.scroll_Div }>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
+                    <div className={ styles.drag_div }>
+                        <DragIcon type="primary" />
+                        <ConstructorElement
+                            text="Биокотлета из марсианской Магнолии"
+                            price={100}
+                            thumbnail="https://code.s3.yandex.net/react/code/meat-01.png"
+                        />
+                    </div>
                 </div>
                 
                 <div className={ styles.drag_div }>
